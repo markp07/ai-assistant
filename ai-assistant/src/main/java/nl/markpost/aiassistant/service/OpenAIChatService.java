7913c3302bf6e2468a0b33d135f91fd2453ce0f7
Mark Post <mark.post@live.nl>
2024-09-13 21:31:25 +0200

    Added OpenAI Added python script to chat

--- ai-assistant/src/main/java/nl/markpost/aiassistant/service/OpenAIChatService.java
+++ ai-assistant/src/main/java/nl/markpost/aiassistant/service/OpenAIChatService.java
@@ -2,29 +2,37 @@ package nl.markpost.aiassistant.service;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
+import dev.langchain4j.service.AiServices;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
-public class ChatServiceImpl implements ChatService {
+public class OpenAIChatService implements ChatService {
 
   @Value("${openai.api-key}")
   private String apiKey;
 
-  public String chat(String input) {
+  public String chat(String message) {
     OpenAiChatModel model = OpenAiChatModel.builder()
         .apiKey(apiKey)
         .modelName(OpenAiChatModelName.GPT_3_5_TURBO)
         .temperature(0.5)
         .build();
 
+    //TODO: create chat history
 
-    return model.generate(input);
+    Assistant assistant = AiServices.builder(Assistant.class).chatLanguageModel(model).build();
+
+    return assistant.chat(message);
   }
 
   @Override
   public Flux<String> chatStream(String input) {
     throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  interface Assistant {
+    String chat(String message);
   }
 }
--- ai-assistant/src/test/java/nl/markpost/aiassistant/service/OpenAIChatServiceTest.java
+++ ai-assistant/src/test/java/nl/markpost/aiassistant/service/OpenAIChatServiceTest.java
@@ -5,26 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-class ChatServiceImplTest {
+class OpenAIChatServiceTest {
 
   @Test
   void testChat() {
-    ChatServiceImpl chatServiceImpl = new ChatServiceImpl();
+    OpenAIChatService openAIChatService = new OpenAIChatService();
     String input = "World";
     String expectedOutput = "Hello, World!";
-    String actualOutput = chatServiceImpl.chat(input);
+    String actualOutput = openAIChatService.chat(input);
     assertEquals(expectedOutput, actualOutput);
   }
 
   @Test
   void testChatStream() {
-    ChatServiceImpl chatServiceImpl = new ChatServiceImpl();
+    OpenAIChatService openAIChatService = new OpenAIChatService();
     String input = "World";
 
     assertThrows(
         UnsupportedOperationException.class,
         () -> {
-          chatServiceImpl.chatStream(input);
+          openAIChatService.chatStream(input);
         });
   }
 }
