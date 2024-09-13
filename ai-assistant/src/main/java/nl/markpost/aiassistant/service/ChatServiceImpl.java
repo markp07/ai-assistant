275898aae659935da335f2c3bad2e83f9e888f9a
Mark Post <mark.post@live.nl>
2024-09-13 21:15:29 +0200

    Added OpenAI

--- ai-assistant/src/main/java/nl/markpost/aiassistant/service/ChatServiceImpl.java
+++ ai-assistant/src/main/java/nl/markpost/aiassistant/service/ChatServiceImpl.java
@@ -1,13 +1,26 @@
 package nl.markpost.aiassistant.service;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
 public class ChatServiceImpl implements ChatService {
 
+  @Value("${openai.api-key}")
+  private String apiKey;
+
   public String chat(String input) {
-    return "Hello, " + input + "!";
+    OpenAiChatModel model = OpenAiChatModel.builder()
+        .apiKey(apiKey)
+        .modelName(OpenAiChatModelName.GPT_3_5_TURBO)
+        .temperature(0.5)
+        .build();
+
+
+    return model.generate(input);
   }
 
   @Override
--- http/postChat.sh
+++ http/postChat.sh
@@ -1,3 +1,3 @@
 curl -X POST http://localhost:8080/api/v1/chat \
      -H "Content-Type: application/json" \
-     -d '{"chat": "Hello, AI Assistant!"}'
+     -d '{"chat": "What can I do in Malaga?"}'
