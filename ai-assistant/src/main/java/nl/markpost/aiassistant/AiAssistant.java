5d6c3eb4d5ed9a9bf35ab2909e6cb45936068ae0
Mark Post <mark.post@live.nl>
2024-09-13 19:36:18 +0200

    Updated pom files -- Added google style check, update jacoco plugin Reformat files Uodated maven tyml

--- ai-assistant/src/main/java/nl/markpost/aiassistant/AiAssistant.java
+++ ai-assistant/src/main/java/nl/markpost/aiassistant/AiAssistant.java
@@ -7,12 +7,13 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 /**
  * The main class of the application.
  *
- * This class is responsible for starting the Spring Boot application.
+ * <p>This class is responsible for starting the Spring Boot application.
  *
- * The {@link SpringBootApplication} annotation is used to enable the auto-configuration of the Spring application context.
- * The {@link ServletComponentScan} annotation is used to scan for Servlet components.
+ * <p>The {@link SpringBootApplication} annotation is used to enable the auto-configuration of the
+ * Spring application context. The {@link ServletComponentScan} annotation is used to scan for
+ * Servlet components.
  *
- * The main method starts the Spring Boot application.
+ * <p>The main method starts the Spring Boot application.
  *
  * @see SpringBootApplication
  * @see ServletComponentScan
@@ -21,8 +22,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 public class AiAssistant {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AiAssistant.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(AiAssistant.class, args);
+  }
 }
