8ab91635913dc4ff024b5f688fc6369c97d34849
Mark Post <mark.post@live.nl>
2024-09-14 12:42:37 +0200

    Update OpenAIChatService and test cases to use mock integratio

--- ai-assistant/src/main/java/nl/markpost/aiassistant/service/OpenAIChatService.java
+++ ai-assistant/src/main/java/nl/markpost/aiassistant/service/OpenAIChatService.java
@@ -1,5 +1,8 @@
 package nl.markpost.aiassistant.service;
 
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
 import dev.langchain4j.service.AiServices;
@@ -20,9 +23,15 @@ public class OpenAIChatService implements ChatService {
         .temperature(0.5)
         .build();
 
-    //TODO: create chat history
+    ChatMemory memory = MessageWindowChatMemory.builder().maxMessages(10)
+        .chatMemoryStore(new CustomChatMemoryStore()).build();
 
-    Assistant assistant = AiServices.builder(Assistant.class).chatLanguageModel(model).build();
+    if (memory.messages().isEmpty()) {
+      memory.add(SystemMessage.systemMessage(
+          "You are a personal assistant. You are here to help answering questions and retrieve information. Keep your answers short and to the point unless asked otherwise.If asked for the weather, don't make up a response, but search it online."));
+    }
+
+    Assistant assistant = AiServices.builder(Assistant.class).chatLanguageModel(model).chatMemory(memory).build();
 
     return assistant.chat(message);
   }
@@ -33,6 +42,7 @@ public class OpenAIChatService implements ChatService {
   }
 
   interface Assistant {
+
     String chat(String message);
   }
 }
--- ai-assistant/src/test/java/nl/markpost/aiassistant/service/OpenAIChatServiceTest.java
+++ ai-assistant/src/test/java/nl/markpost/aiassistant/service/OpenAIChatServiceTest.java
@@ -2,14 +2,42 @@ package nl.markpost.aiassistant.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 class OpenAIChatServiceTest {
 
+  @Mock
+  private OpenAiChatModel openAiChatModel;
+
+  @Mock
+  private OpenAIChatService.Assistant assistant;
+
+  @InjectMocks
+  private OpenAIChatService openAIChatService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    ChatResponse response = Mockito.mock(ChatResponse.class);
+//    when(response.()).thenReturn("Hello, World!");
+    when(openAiChatModel.chat(any(ChatRequest.class))).thenReturn(response);
+    when(assistant.chat(anyString())).thenReturn("Hello, World!");
+  }
+
   @Test
   void testChat() {
-    OpenAIChatService openAIChatService = new OpenAIChatService();
     String input = "World";
     String expectedOutput = "Hello, World!";
     String actualOutput = openAIChatService.chat(input);
@@ -18,7 +46,6 @@ class OpenAIChatServiceTest {
 
   @Test
   void testChatStream() {
-    OpenAIChatService openAIChatService = new OpenAIChatService();
     String input = "World";
 
     assertThrows(
